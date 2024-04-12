06e0e15d47392c2576d9b9b39164be4ea1d4df7e
HB74C21 <6720@holbertonstudents.com>
2024-04-12 17:57:29 +0000

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,9 +2,11 @@
 """
 Script that starts a Flask web application.
 """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
@@ -12,14 +14,15 @@ app = Flask(__name__)
 def states_list():
     """Route handler to display a list of states."""
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def close(self):
+def teardown(exception):
     """Close the current SQLAlchemy Session after each request."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
